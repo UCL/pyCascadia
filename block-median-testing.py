42199811a51a791216df61998b936706cfd2e318
alessandrofelder <a.felder@ucl.ac.uk>
2021-03-02 17:47:00 +0000

    more effective manipulation of dataframe

--- block-median-testing.py
+++ block-median-testing.py
@@ -1,30 +1,30 @@
+# This script should not be used in any serious way (in its current form at least)!
+# It's just a way for us to learn how to use pyGMT and how to read/write the sample data.
+
 from pygmt import blockmedian
 import pandas as pd
 import xarray as xr
 import matplotlib.pyplot as plt
 
+# hard-coded Windows filepath :scream !
 filepath = "C:\\Users\\Alessandro\\Documents\\UCL-projects\\bathymetry-sample-data\\gebco_2019.nc"
 
 xr_data = xr.open_dataarray(filepath)
 
-lats = 300
-lons = 300
+# we might lose some metadata with the `to_dataframe()` conversion here, but that's OK as we'll just want a bunch of xyz
+# independent of the original data file here in the future. Good to keep in mind, though, and ensure that the metadata
+# is correct and in the right unit etc.
+xyz_data = xr_data.to_dataframe()
+xyz_data = xyz_data.reset_index()
+xyz_data.rename(columns={'lon': 'x', 'lat': 'y', 'elevation': 'z'}, inplace=True)
+xyz_data = xyz_data[['x', 'y', 'z']]  # `blockmedian()` requires columns in the right order!!!
 
-# let's do this in a way where we don't create a super big list!
-data = {'x': [], 'y': [], 'z': []}
-for lat_index in range(lats):
-    for lon_index in range(lons):
-        data['x'].append(xr_data.coords['lat'].data[6000-lon_index])
-        data['y'].append(xr_data.coords['lon'].data[lat_index])
-        data['z'].append(xr_data.data[lat_index, lon_index])
-
-pd_data = pd.DataFrame(data)
-region = [min(data['x']), max(data['x']), min(data['y']), max(data['y'])]
-bmd = blockmedian(pd_data, spacing=0.05, region=region)
+region = [-127, -122, 47, 49]  # area of interest
+bmd = blockmedian(xyz_data, spacing=0.05, region=region)  # TODO what unit is the spacing???
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(xs=data['x'], ys=data['y'], zs=data['z'], s=0.1)
+# ax.scatter(xs=xyz_data['x'], ys=xyz_data['y'], zs=xyz_data['z'], s=0.1)
 ax.scatter(xs=bmd['x'], ys=bmd['y'], zs=bmd['z'])
 
 plt.show()
