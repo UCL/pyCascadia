c4b1821c4eea4078c63e4a909e1bc1ee9b9ae39d
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-03-03 16:53:32 +0000

    Implement generic loading function for netcfd and geotiff files into dataframes suitable for GMT processing

--- block-median-testing.py
+++ block-median-testing.py
@@ -8,12 +8,22 @@ It's just a way for us to learn how to use pyGMT and how to read/write the sampl
 from pygmt import blockmedian
 import pandas as pd
 import xarray as xr
+import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 import rasterio
 from rasterio.plot import show
 
 def load_source(filepath):
+    ext = filepath.split('.')[-1]
+    if ext == 'nc':
+        return load_netcfd(filepath)
+    elif ext == 'tif':
+        return load_geotiff(filepath)
+    else:
+        raise RuntimeError(f"Error: filetype {ext} not recognised.")
+
+def load_netcfd(filepath):
     xr_data = xr.open_dataarray(filepath)
     # we might lose some metadata with the `to_dataframe()` conversion here, but that's OK as we'll just want a bunch of xyz
     # independent of the original data file here in the future. Good to keep in mind, though, and ensure that the metadata
@@ -25,12 +35,54 @@ def load_source(filepath):
     return xyz_data
 
 def load_geotiff(filepath):
+    """Loads geotiff file as GMT-consumable pandas dataframe"""
+
     print(f"Opening {filepath}")
     dataset = rasterio.open(filepath)
     print(f"Number of bands: {dataset.count}")
     print(f"Resolution: {dataset.width}, {dataset.height}")
     print(f"CRS: {dataset.crs}")
-    return dataset
+
+    bounds = dataset.bounds
+    left = bounds.left
+    right = bounds.right
+    top = bounds.top
+    bottom = bounds.bottom
+
+    x_res = dataset.width
+    y_res = dataset.height
+
+    # Get positions of upper-right corner of pixels
+    x = np.linspace(left, right, x_res)
+    y = np.linspace(top, bottom, y_res)
+
+    # Recentre pixel locations
+    dx = abs(right - left)/(x_res-1)
+    dy = abs(top - bottom)/(y_res-1)
+    x += dx/2
+    y -= dy/2
+
+    # This is a test
+    # ix = 0
+    # iy = 0
+
+    # pt = dataset.xy(ix, iy)
+    # x_error = abs(x[ix, iy] - pt[0])
+    # y_error = abs(y[ix, iy] - pt[1])
+
+    # print(x_error, y_error)
+
+    # Form points
+    x, y = np.meshgrid(x, y)
+    z = dataset.read()
+
+    # Convert to dataframe
+    df = pd.DataFrame()
+    df['x'] = x.flatten()
+    df['y'] = y.flatten()
+    df['z'] = z.flatten()
+
+    return df
 
 def main():
     # Handle arguments
@@ -42,8 +94,8 @@ def main():
     filenames = args.filenames
     filepath = filenames[0]
 
-    dataset = load_geotiff(filepath)
-    show(dataset)
+    xyz_data = load_source(filepath)
+    print(xyz_data)
 
     # xyz_data = load_source(filepath)
 
