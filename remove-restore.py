b72cd9897f622709c19106d3ff6c031299d2ba63
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-03-22 18:02:52 +0000

    Use base grid without regridding

--- remove-restore.py
+++ remove-restore.py
@@ -32,11 +32,15 @@ def form_grid(xyz_data, region=None, spacing=None):
 def region_to_str(region):
     return '/'.join(map(str, region))
 
+def extract_region(grid):
+    return [float(grid.lon[0]), float(grid.lon[-1]), float(grid.lat[0]), float(grid.lat[-1])]
+
 def main():
     # Handle arguments
     parser = argparse.ArgumentParser(description='Combine multiple bathymmetry sources into a single grid')
     parser.add_argument('filenames', nargs='+', help='sources to combine with the base grid')
     parser.add_argument('--base', required=True, help='base grid')
+    parser.add_argument('--regrid_base', action='store_true', help='base grid')
     args = parser.parse_args()
 
     filenames = args.filenames
@@ -48,16 +52,22 @@ def main():
     # Create base grid
     # base_region = [-126, -122, 47, 49.5]  # area of interest
     base_region = [-125, -122, 48, 49]  # area of interest
-    spacing = 0.005
+    # spacing = 18/3600 # in degrees
 
-    base_xyz_data = load_source(base_filepath, plot=False)
-    print("Creating base grid")
-    base_grid = form_grid(base_xyz_data, region=base_region, spacing=spacing)
+    # base_xyz_data = load_source(base_filepath, plot=False)
+    # print("Creating base grid")
+    # base_grid = form_grid(base_xyz_data, region=base_region, spacing=spacing)
+
+    base_grid = load_source(base_filepath, plot=False)
+    base_grid = grdcut(base_grid, region=base_region)
+    print(base_grid.lat)
+
+    spacing = float(base_grid.lat[1] - base_grid.lat[0])
 
     print("Loading update grid")
     xyz_data = load_source(filepath, plot=False)
     # region = [-125, -122, 48, 49] # TODO replace with automatic calc
-    region = base_region
+    region = extract_region(base_grid)
     xyz_data.where(xyz_data['z'] != nodata_val, inplace=True)
 
     print("Blockmedian update grid")
@@ -77,7 +87,8 @@ def main():
     diff.to_csv(diff_xyz_fname, sep=' ', header=False, index=False)
 
     os.system(f'gmt nearneighbor {diff_xyz_fname} -G{diff_grid_fname} -R{region_to_str(region)} -I{spacing} -S{spacing} -N4 -E0 -V')
-    large_diff_grid = grdcut(diff_grid_fname, region=base_region, extend=0.0)
+    large_diff_grid = grdcut(diff_grid_fname, region=region, extend=0.0)
+    print(large_diff_grid.y)
 
     print("Update base grid")
     base_grid.values += large_diff_grid.values
@@ -85,8 +96,8 @@ def main():
     fig, axes = plt.subplots(2,2)
     base_grid.plot(ax=axes[0,0])
     large_diff_grid.plot(ax=axes[0,1])
-    base_grid.differentiate('x').plot(ax=axes[1,0])
-    base_grid.differentiate('y').plot(ax=axes[1,1])
+    # base_grid.differentiate('x').plot(ax=axes[1,0])
+    # base_grid.differentiate('y').plot(ax=axes[1,1])
     plt.show()
 
 if __name__ == "__main__":
