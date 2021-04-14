9f78aa665ca614a3617cc2220bc7f78dea3e4a79
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-04-14 14:37:56 +0100

    Remove unused option

--- contour_generator/close_boundary.py
+++ contour_generator/close_boundary.py
@@ -3,7 +3,7 @@
 from loaders import load_source
 import argparse
 
-def replace_with_zeros(arr, test_arr=None):
+def replace_land_with_zeros(arr, test_arr=None):
     if test_arr is None:
         test_arr = arr
 
@@ -15,26 +15,22 @@ def main():
     parser = argparse.ArgumentParser(description='Add halo surrounding given netcdf file')
     parser.add_argument('--input', required=True, help='input file')
     parser.add_argument('--output', required=True, help='output file')
-    parser.add_argument('--inplace', action='store_true', default=True, help='replace boundary values')
     parser.add_argument('--offset', action='store_true', default=False, help='use one index from true boundary as boundary')
     args = parser.parse_args()
 
     in_fname = args.input
     input_grid = load_source(in_fname, plot=False)
 
-    if args.inplace:
-        if args.offset:
-            replace_with_zeros(input_grid[0,:], input_grid[1,:])
-            replace_with_zeros(input_grid[-1,:], input_grid[-2,:])
-            replace_with_zeros(input_grid[:,0], input_grid[:,1])
-            replace_with_zeros(input_grid[:,-1], input_grid[:,-2])
-        else:
-            replace_with_zeros(input_grid[0,:])
-            replace_with_zeros(input_grid[-1,:])
-            replace_with_zeros(input_grid[:,0])
-            replace_with_zeros(input_grid[:,-1])
+    if args.offset:
+        replace_land_with_zeros(input_grid[0,:], input_grid[1,:])
+        replace_land_with_zeros(input_grid[-1,:], input_grid[-2,:])
+        replace_land_with_zeros(input_grid[:,0], input_grid[:,1])
+        replace_land_with_zeros(input_grid[:,-1], input_grid[:,-2])
     else:
-        raise NotImplementedError("Adding an additionl halo is not supported yet")
+        replace_land_with_zeros(input_grid[0,:])
+        replace_land_with_zeros(input_grid[-1,:])
+        replace_land_with_zeros(input_grid[:,0])
+        replace_land_with_zeros(input_grid[:,-1])
 
     input_grid.to_netcdf(args.output)
 
