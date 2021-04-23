230a83d29a9f6df1132fbfb8ab1b05c3506887f8
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-04-23 08:58:29 +0100

    Rename variables in test from df_ to xr_ to reflect the actual file type

--- test_loaders.py
+++ test_loaders.py
@@ -2,13 +2,13 @@ import pytest
 import loaders
 
 def test_loader_equivalence():
-    df_geotiff, _, _ = loaders.load_source("./test_data/small_sample.tif")
-    df_netcdf, _, _ = loaders.load_source("./test_data/small_sample.nc")
+    xr_geotiff, _, _ = loaders.load_source("./test_data/small_sample.tif")
+    xr_netcdf, _, _ = loaders.load_source("./test_data/small_sample.nc")
 
     # geotiff and netcdf don't order the rows in the same way, so we sort for comparison
     # index ignored because otherwise they row index persists after sorting, and comparison doesn't work.
-    sorted_geotiff = df_geotiff.sortby([df_geotiff.x, df_geotiff.y])
-    sorted_netcdf = df_netcdf.sortby([df_netcdf.x, df_netcdf.y]) 
+    sorted_geotiff = xr_geotiff.sortby([xr_geotiff.x, xr_geotiff.y])
+    sorted_netcdf = xr_netcdf.sortby([xr_netcdf.x, xr_netcdf.y]) 
 
     diff_x = abs(sorted_geotiff.x - sorted_netcdf.x)
     diff_y = abs(sorted_geotiff.y - sorted_netcdf.y)
