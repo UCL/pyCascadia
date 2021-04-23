636ecb7ed97a1295e0d92c99c0ee1a92edb60a0c
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-04-23 09:22:10 +0100

    Add grid tests

---
+++ test_grid.py
@@ -0,0 +1,29 @@
+import pytest
+import os
+from grid import Grid
+from xarray.testing import assert_equal, assert_allclose
+
+def test_grid_loading():
+    nc_fname = './test_data/small_sample.nc'
+    tif_fname = './test_data/small_sample.tif'
+
+    grid_nc = Grid(nc_fname)
+    grid_tif = Grid(tif_fname)
+
+    assert grid_nc.spacing == grid_tif.spacing
+    assert grid_nc.region == grid_tif.region
+
+    assert_allclose(grid_nc.grid, grid_tif.grid)
+
+def test_grid_saving():
+    nc_fname = './test_data/small_sample.nc'
+    nc_fname_save = './test_data/small_sample_temp.nc'
+
+    grid_og = Grid(nc_fname)
+    grid_og.save_grid(nc_fname_save)
+
+    grid_saved = Grid(nc_fname)
+
+    assert_equal(grid_og.grid, grid_saved.grid)
+
+    os.remove(nc_fname_save)
