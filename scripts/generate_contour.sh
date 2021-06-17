9f973caee3bc72dce91ab56aec150ef9adc451d5
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-06-17 10:13:46 +0100

    Add level parameter to GRASS contour extraction tool, to allow the selection of non-zero contours

--- scripts/generate_contour.sh
+++ scripts/generate_contour.sh
@@ -4,9 +4,10 @@ export GRASS_MESSAGE_FORMAT=plain
 
 input_fname=$1
 output_fname=$2
+level=$3
 
 r.in.gdal input=NETCDF:"$input_fname" output=elevation -o --overwrite
-r.contour in=elevation out=contours levels=0 cut=200 --overwrite
+r.contour in=elevation out=contours levels=$level cut=200 --overwrite
 v.out.ogr format=ESRI_Shapefile input=contours output=$output_fname --overwrite
 
 # For plotting
