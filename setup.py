f5c6ee9f2795e1433daed82a3b9fbd72d0b60957
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-08-04 15:25:33 +0100

    Add readme to setup metadata

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
+import pathlib
 from setuptools import setup
 
-setup()
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    long_description=README,
+    long_description_content_type="text/markdown",
+)
